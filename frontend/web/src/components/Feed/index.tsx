9ec18731b5016a2a95b43e237aee34f38ea755f1
aspli <alexandre.xoquito@gmail.com>
2022-12-09 09:22:26 -0300

    [fix] post likes

--- frontend/web/src/components/Feed/index.tsx
+++ frontend/web/src/components/Feed/index.tsx
@@ -9,22 +9,25 @@ interface Post {
         _id: string,
         title: string,
         description: string,
-        profile: Profile,
+        profile: {
+            name: string
+        },
         comments: [],
         likes: []
 }
 
-interface Profile{
+/* interface Profile{
         _id: string,
         name: string,
         user: string,
         following: [],
         followers: []
-}
+} */
 
 function Feed(){
-    const [ posts, setPosts] = useState<Post[]>([])
+
     const token = localStorage.getItem("accessToken")
+    const [ posts, setPosts] = useState<Post[]>([])    
     const profile = localStorage.getItem("profile")
     const user = localStorage.getItem("user")
     const authHeader = {
@@ -48,20 +51,20 @@ function Feed(){
     
     async function handleLike(postId: String){
         try {
-            await api.post(`/posts/${postId}/like`, null, authHeader)
-            const newPost = posts
-                .filter((post) => post._id === postId)
-                .map((post) => { 
-                    post.likes.push(profile)
-                    return post 
-                })
+                await api.post(`/posts/${postId}/like`, null, authHeader)
+                const newPost = posts
+                    .filter((post) => post._id === postId)
+                    .map((post) => { 
+                        post.likes.push(profile)
+                        return post 
+                    })
 
-            /* setPosts((postss) => {
+            setPosts((posts) => {
                 const post = newPost[0]
-                const = postss.indexOf(post)
+                const index = posts.indexOf(post)
                 posts[index] = post
                 return [ ...posts]
-            }) */
+            })
         } catch (err) {
             console.error(err)
         }
@@ -77,16 +80,18 @@ function Feed(){
                 </div>
             </Heading>
             <section>
-                { posts && posts.map((post: Post) => (
-       
+                { posts && posts.map((post: Post) => (       
                     <div className="border-b border-slate-400 mt-4" key={post._id}>
                         <div className="flex flex-row items-center ml-5 my-4">
                             <FaRegUserCircle size={48} height="light" className="text-slate-100" />
                             <Text className="font-extrabold ml-2">{post.profile.name}</Text>
                         </div>
-                        <Text asChild className="ml-16">
-                            <p>{post.description}</p>
-                        </Text>
+                        <div className="ml-16 flex flex-col gap-2">
+                            <Heading size="sm">{post.title}</Heading>
+                            <Text asChild >
+                                <p>{post.description}</p>
+                            </Text>
+                        </div>                        
                         <div className="flex items-center ml-16 my-4 space-x-2">
                             <FaRocketchat size={24} className="text-slate-50" />
                             <Text size="sm">{post.comments.length}</Text>
