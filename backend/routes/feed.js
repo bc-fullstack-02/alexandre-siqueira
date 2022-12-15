0ccdfc658440f4de07b719f958b49a0b4fa1a278
aspli <alexandre.xoquito@gmail.com>
2022-12-15 16:37:42 -0300

    modified  order recent first

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -6,7 +6,7 @@ feedRouter.route("/")
   .get((req, res, next) => Promise.resolve()
     .then(() => Profile.findById(req.user.profile.id))
     .then((profile) => Post.find({ profile: { $in: profile.following } }).populate("profile"))
-    .then((data) => res.status(200).json(data))
+    .then((data) => res.status(200).json(data.sort(function(a,b){ return new Date(b.createAt) - new Date(a.createAt)})))
     .catch((err) => next(err))
 );
 
